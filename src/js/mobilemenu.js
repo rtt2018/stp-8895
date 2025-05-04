357dbe9f8871a1651a6c726a8e0ba0f1e4d0699f
Taras <r.taras.t.1@gmail.com>
2025-05-05 00:30:41 +0300

    add visibility on nav link

--- src/js/mobilemenu.js
+++ src/js/mobilemenu.js
@@ -2,13 +2,16 @@ const openBtnEl = document.querySelector('[data-class="burger-btn"]');
 const closeBtnEl = document.querySelector('[data-type="mobile-menu-closeBtn"]');
 const burgerMenuEl = document.querySelector('[data-type="mobile-menu-overlay"]');
 
-openBtnEl.addEventListener('click', e => {
+openBtnEl.addEventListener('click', openMobileMenu);
+closeBtnEl.addEventListener('click', closeMobileMenu);
+
+function closeMobileMenu() {
+    burgerMenuEl.dataset.action = 'isClose';
+    document.documentElement.dataset.scrollLock = false;
+}
+
+function openMobileMenu() {
     burgerMenuEl.dataset.action = 'isOpen';
     document.documentElement.dataset.scrollLock = true;
 
-});
-
-closeBtnEl.addEventListener('click', e => {
-    burgerMenuEl.dataset.action = 'isClose';
-    document.documentElement.dataset.scrollLock = false;
-});
+}
