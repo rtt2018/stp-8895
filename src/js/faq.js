d028c8b2996ec149dab46146a922149645e0dc7c
Taras <r.taras.t.1@gmail.com>
2025-05-06 23:12:53 +0300

    upd faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,7 +3,7 @@ spoilerList.addEventListener('click', openSpoiler);
 
 function openSpoiler(event) {
     const tagName = event.target.tagName;
-    if (tagName === 'BUTTON' || tagName === 'PICTURE' || tagName === 'IMG') {
+    if (tagName === 'BUTTON' || tagName === 'svg' || tagName === 'use' || tagName === 'SVG' || tagName === 'USE') {
         const listItem = event.target.closest('li');
         const isOpenSpoiler = listItem.dataset.isOpen === "true";
         listItem.dataset.isOpen = String(!isOpenSpoiler);
