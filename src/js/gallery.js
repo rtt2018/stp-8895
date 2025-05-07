69afc87ad24d80ec2fd7297e7f4ccf332f773f53
Taras <r.taras.t.1@gmail.com>
2025-05-07 23:28:39 +0300

    add reviews

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,38 +1,38 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const gallerySwiper = new Swiper('.gallery-swiper', {
-    direction: 'horizontal',
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+// document.addEventListener('DOMContentLoaded', function () {
+//   const gallerySwiper = new Swiper('.gallery-swiper', {
+//     direction: 'horizontal',
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
 
-    slidesPerView: 'auto',
-    spaceBetween: 15,
-    centeredSlides: true,
-    effect: 'slide',
+//     slidesPerView: 'auto',
+//     spaceBetween: 15,
+//     centeredSlides: true,
+//     effect: 'slide',
 
-    breakpoints: {
-      320: {
-        slidesPerView: 'auto',
-        spaceBetween: 10,
-        centeredSlides: true,
-        effect: 'slide',
-      },
-      1020: {
-        slidesPerView: 'auto',
-        spaceBetween: 30,
-        centeredSlides: true,
-        effect: 'cards',
-        cardsEffect: {
-          perSlideOffset: 10,
-          perSlideRotate: 2,
-          rotate: true,
-          slideShadows: true,
-        },
-      },
-    },
-  });
-});
+//     breakpoints: {
+//       320: {
+//         slidesPerView: 'auto',
+//         spaceBetween: 10,
+//         centeredSlides: true,
+//         effect: 'slide',
+//       },
+//       1020: {
+//         slidesPerView: 'auto',
+//         spaceBetween: 30,
+//         centeredSlides: true,
+//         effect: 'cards',
+//         cardsEffect: {
+//           perSlideOffset: 10,
+//           perSlideRotate: 2,
+//           rotate: true,
+//           slideShadows: true,
+//         },
+//       },
+//     },
+//   });
+// });
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,9 +1,42 @@
-import Swiper from "swiper"
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-const swiperReviews = new Swiper('[data-swiper-container]', {
-    slideClass: 'reviews-slide-item',
+
+const swiperContainer = document.querySelector('[data-swiper]');
+
+const swiperWrapperElement = document.querySelector('[data-swiper-wrapper]');
+
+const slideElements = document.querySelector('[data-swiper-slide]');
+
+const paginationElement = document.querySelector('[data-swiper-pagination]');
+
+const swiperReviews = new Swiper(swiperContainer, {
+    slideClass: slideElements.classList[0],
+    wrapperClass: swiperWrapperElement.classList[0],
+    direction: "horizontal",
+    slidesPerView: 1,
+    spaceBetween: 32,
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true, // дозволяє кліком переходити по крапках
+        el: paginationElement,
+        clickable: true,
+        bulletClass: 'reviews-swiper-bullet',
+        bulletActiveClass: 'current',
+        renderBullet: function (index, className) {
+            return `<div class="${className}" data-swiper-pagination-bullet data-bullet-number="${index}"></div>`;
+        },
     },
+    navigation: {
+        nextEl: '[data-swipe-right]',
+        prevEl: '[data-swipe-left]',
+    },
+    breakpoints: {
+        320: {
+            navigation: {
+                enabled: false,
+            },
+        },
+        1200: {
+            pagination: false,
+        },
+    }
 });
