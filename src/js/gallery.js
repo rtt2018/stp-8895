146a0d38e7f1f85869add8d7cca967e257b5659e
Tetiana Viienko <112611578+Vitavanika@users.noreply.github.com>
2025-05-08 15:10:37 +0200

    Gallery (#38)
    * Add gallery images and implement Swiper for enhanced image display
    
    * Add new desktop gallery images for enhanced visual content
    
    * Add new airplane images and update sprite.svg with new icons
    
    * Remove unused icon-swipe from sprite.svg and update reviews
    
    * Refactor gallery swiper configuration and styles for improved responsiveness

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,69 +3,77 @@ import 'swiper/css/bundle';
 
 const swiperGalleryContainer = document.querySelector('[data-gallery-swiper]');
 
-const swiperGalleryWrapperElement = document.querySelector('[data-gallery-swiper-wrapper]');
+const swiperGalleryWrapperElement = document.querySelector(
+  '[data-gallery-swiper-wrapper]'
+);
 
 const slideGalleryElements = document.querySelector('[data-gallery-slide]');
 
-const paginationGalleryElement = document.querySelector('[data-gallery-pagination]');
-
+const paginationGalleryElement = document.querySelector(
+  '[data-gallery-pagination]'
+);
 
 document.addEventListener('DOMContentLoaded', function () {
-    const gallerySwiper = new Swiper(swiperGalleryContainer, {
-        slideClass: slideGalleryElements.classList[0],
-        wrapperClass: swiperGalleryWrapperElement.classList[0],
-        direction: 'horizontal',
-        grabCursor: true,
-        loop: true,
-        centeredSlides: false,
-        slidesPerView: "auto",
-        spaceBetween: 28,
-        effect: "coverflow",
-        coverflowEffect: {
-            rotate: 0,
-            stretch: 10,    // трохи зсуває слайди один на одного
-            depth: 100,
-            modifier: 4,
-            slideShadows: false,
-        },
+  const gallerySwiper = new Swiper(swiperGalleryContainer, {
+    slideClass: slideGalleryElements.classList[0],
+    wrapperClass: swiperGalleryWrapperElement.classList[0],
+    direction: 'horizontal',
+    grabCursor: true,
+    loop: true,
+    loopedSlides: 5,
+    centeredSlides: false,
+    slidesPerView: 'auto',
+    spaceBetween: window.innerWidth < 1200 ? -320 : 16,
+    effect: window.innerWidth >= 1200 ? 'coverflow' : 'slides',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 25,
+      depth: 100,
+      modifier: 4,
+      slideShadows: false,
+    },
 
-        pagination: {
-            el: paginationGalleryElement,
-            clickable: true,
-            bulletClass: 'gallery-swiper-bullet',
-            bulletActiveClass: 'current',
-            renderBullet: function (index, className) {
-                return `<div class="${className}" data-swiper-pagination-bullet data-bullet-number="${index}"></div>`;
-            },
-        },
+    pagination: {
+      el: paginationGalleryElement,
+      clickable: true,
+      bulletClass: 'gallery-swiper-bullet',
+      bulletActiveClass: 'current',
+      renderBullet: function (index, className) {
+        return `<div class="${className}" data-swiper-pagination-bullet data-bullet-number="${index}"></div>`;
+      },
+    },
+    navigation: {
+      nextEl: '[data-gallery-swipe-right]',
+      prevEl: '[data-gallery-swipe-left]',
+    },
+    breakpoints: {
+      320: {
         navigation: {
-            nextEl: '[data-gallery-swipe-right]',
-            prevEl: '[data-gallery-swipe-left]',
+          enabled: false,
         },
-        breakpoints: {
-            320: {
-                navigation: {
-                    enabled: false,
-                },
-            },
-            1020: {
-                pagination: false,
-            },
-        },
-        on: {
-            slideChange: function () {
-                const activeSlide = this.slides[this.activeIndex];
-                const activeSlideNumber = Number.parseInt(activeSlide.dataset.slideNumber)
-                const descriptionList = document.querySelector('[data-gallery-description-list]');
-                const currentVisible = descriptionList.querySelector('[data-is-visible="true"]')
-                currentVisible.dataset.isVisible = String(false);
-                const nextVisibleDesc = descriptionList.querySelector(`[data-description-number="${activeSlideNumber}"]`)
-                nextVisibleDesc.dataset.isVisible = true;
-            }
-        }
-    });
+      },
+      1200: {
+        pagination: false,
+      },
+    },
+    on: {
+      slideChange: function () {
+        const activeSlide = this.slides[this.activeIndex];
+        const activeSlideNumber = Number.parseInt(
+          activeSlide.dataset.slideNumber
+        );
+        const descriptionList = document.querySelector(
+          '[data-gallery-description-list]'
+        );
+        const currentVisible = descriptionList.querySelector(
+          '[data-is-visible="true"]'
+        );
+        currentVisible.dataset.isVisible = String(false);
+        const nextVisibleDesc = descriptionList.querySelector(
+          `[data-description-number="${activeSlideNumber}"]`
+        );
+        nextVisibleDesc.dataset.isVisible = true;
+      },
+    },
+  });
 });
-
-/**
- * 1. Зробити зміну ткесту у підписі
- * 2.  **/
