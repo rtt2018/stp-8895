6fb1d9569209fef747fb7bbc87e88d240accfdf7
Vitavanika <vitavanika@gmail.com>
2025-05-09 16:51:54 +0200

    Refactor and clean up code by removing unused files and updating HTML structure for better accessibility and responsiveness

--- src/js/header.js
+++ src/js/header.js
@@ -1,26 +1,49 @@
-const sections = document.querySelectorAll('section');
-const navHeaderList = document.querySelector('[data-nav-header-list]')
+const navHeaderList = document.querySelector('[data-nav-header-list]');
 const mobileMenuNavList = document.querySelector('[data-mobile-nav]');
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(
+  entries => {
     entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const visibleSectionId = entry.target.id;
-            const currentSectionVisible = navHeaderList.querySelector('[data-visibility="true"]')
-            currentSectionVisible.dataset.visibility = 'false';
-            const nextSectionVisible = navHeaderList.querySelector(`[data-section-id="${visibleSectionId}"]`)
-            nextSectionVisible.dataset.visibility = true;
+      if (entry.isIntersecting) {
+        const visibleSectionId = entry.target.id;
 
-            const currentMobSectVisible = mobileMenuNavList.querySelector('[data-visibility="true"]')
-            currentMobSectVisible.dataset.visibility = 'false';
-            const nextMobSectionVisible = mobileMenuNavList.querySelector(`[data-section-id="${visibleSectionId}"]`)
-            nextMobSectionVisible.dataset.visibility = true;
+        const currentSectionVisible = navHeaderList.querySelector(
+          '[data-visibility="true"]'
+        );
+        if (currentSectionVisible) {
+          currentSectionVisible.dataset.visibility = 'false';
         }
+
+        const nextSectionVisible = navHeaderList.querySelector(
+          `[data-section-id="${visibleSectionId}"]`
+        );
+        if (nextSectionVisible) {
+          nextSectionVisible.dataset.visibility = true;
+        }
+
+        const currentMobSectVisible = mobileMenuNavList.querySelector(
+          '[data-visibility="true"]'
+        );
+        if (currentMobSectVisible) {
+          currentMobSectVisible.dataset.visibility = 'false';
+        }
+
+        const nextMobSectionVisible = mobileMenuNavList.querySelector(
+          `[data-section-id="${visibleSectionId}"]`
+        );
+        if (nextMobSectionVisible) {
+          nextMobSectionVisible.dataset.visibility = true;
+        }
+      }
     });
-}, {
+  },
+  {
     root: null,
     threshold: 0.2,
-    rootMargin: '-25% 0px -25% 0px'
-});
+    rootMargin: '-25% 0px -25% 0px',
+  }
+);
 
-sections.forEach(section => observer.observe(section));
+document
+  .querySelectorAll('section')
+  .forEach(section => observer.observe(section));
